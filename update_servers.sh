de5334ad15aec3d6056c4e5e8ba0c254c011cff7
Kaden <kaden@probotix.com>
2018-09-05 17:40:24 -0500

    added tool scripts not included in zip
    apt_mirror.sh
    restore_gnome.sh
    update_servers.sh

--- update_servers.sh
+++ update_servers.sh
@@ -1,15 +1,6 @@
+#!/bin/bash
+
 sudo sed -i.bak -r 's/(archive|security).ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
 
 #also need to remove us. sources
-
-
-samba
-# downloads 5 package files
-/var/cache/apt/archives/libpam-smbpass_2%3a3.4.7~dfsg-1ubuntu3.15_i386.deb
-/var/cache/apt/archives/libwbclient0_2%3a3.4.7~dfsg-1ubuntu3.15_i386.deb
-/var/cache/apt/archives/samba-common_2%3a3.4.7~dfsg-1ubuntu3.15_all.deb
-/var/cache/apt/archives/samba_2%3a3.4.7~dfsg-1ubuntu3.15_i386.deb
-/var/cache/apt/archives/smbclient_2%3a3.4.7~dfsg-1ubuntu3.15_i386.deb
-
-
-
+sudo sed -i '/us.old-releases.ubuntu.com/c\' /etc/apt/sources.list
