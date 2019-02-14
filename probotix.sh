0b55358d65a88e61c54a987182375c23b72748d3
Kaden <kaden@probotix.com>
2019-02-14 08:44:56 -0600

    increment version number

--- probotix.sh
+++ probotix.sh
@@ -5,7 +5,7 @@ CONFIG_NAME="PROBOTIX Galaxy Series LinuxCNC Configurator"
 #	Originally by Len Shelton
 #	Updated by Kaden Lewis
 #
-_VERSION="3.1.1"
+_VERSION="3.1.2"
 
 ###################################################################################################
 # 	some variables
